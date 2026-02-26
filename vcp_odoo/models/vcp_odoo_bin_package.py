c92e3fba8b2b7a59635c2365144e6d2e571678af
Sylvain LE GAL <sylvain.legal@grap.coop>
2026-02-26 15:04:55 +0100

    [REF] vcp_odoo: create a python file per model, following OCA convention

---
+++ vcp_odoo/models/vcp_odoo_bin_package.py
@@ -0,0 +1,18 @@
+# Copyright 2026 Dixmit
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models, tools
+
+
+class VcpOdooBinPackage(models.Model):
+    _name = "vcp.odoo.bin.package"
+    _description = "Binary Package required by an Odoo Module"
+
+    name = fields.Char(required=True)
+
+    @tools.ormcache("name")
+    def _get_bin(self, name):
+        bin_src = self.search([("name", "=", name)], limit=1)
+        if not bin_src:
+            bin_src = self.create({"name": name})
+        return bin_src.id
