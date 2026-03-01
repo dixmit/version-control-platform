7abefa103701c830df65fc86049236ac2fb8f657
Sylvain LE GAL <sylvain.legal@grap.coop>
2026-03-01 21:12:34 +0100

    [IMP] Allow to unlink modules

--- vcp_odoo/models/vcp_odoo_bin_package.py
+++ vcp_odoo/models/vcp_odoo_bin_package.py
@@ -1,7 +1,8 @@
 # Copyright 2026 Dixmit
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import fields, models, tools
+from odoo import _, api, fields, models, tools
+from odoo.exceptions import UserError
 
 
 class VcpOdooBinPackage(models.Model):
@@ -22,3 +23,13 @@ class VcpOdooBinPackage(models.Model):
         if not bin_src:
             bin_src = self.create({"name": name})
         return bin_src.id
+
+    @api.ondelete(at_uninstall=False)
+    def _check_module_versions(self):
+        if self.mapped("module_version_ids"):
+            raise UserError(
+                _(
+                    "You can not delete packages that are related to Odoo Modules. "
+                    "You should first delete the related odoo modules."
+                )
+            )
